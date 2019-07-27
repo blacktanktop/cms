ee426efb7c149c85f4ab6a0a449fe337e73315f2
blacktanktop <blacktanktopme@gmail.com>
2019-07-27 16:56:09 +0900

    Fix comment

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,14 @@
 #!/bin/bash -eux
+
+# read variable in conf
 source "$(dirname $0)/bin/conf"
 
 [ "$USER" = "root" ]
 
+# remove contents
 rm -rf "${contentsdir:?}"
+# move contents dir
 cd "$wwwdir"
+# clone contents
 git clone "https://github.com/$contents_owner/$contents"
 chown www-data:www-data "$contentsdir" -R
